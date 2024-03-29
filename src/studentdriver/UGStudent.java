0c7ab8973b5525376d95923b1eb1b2fb2d325824
Gabe Goodlet <ggoodlet2004@gmail.com>
2024-03-29 11:43:41 -0500

    GraduateStudent Instance Variables and Methods

--- src/studentdriver/UGStudent.java
+++ src/studentdriver/UGStudent.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package studentdriver;
 
-/**
- *
- * @author S541341
- */
-public class UGStudent {
+public class UGStudent extends StudentFees {
+    private double scholarshipAmount;
+    private int coursesEnrolled;
+    private boolean hasScholarship;
+    private double ADDITIONAL_FEE = 820.70;
     
+    public UGStudent(String studentName, int studentID, boolean isEnrolled, boolean hasScholarship, double scholarshipAmount, int coursesEnrolled){
+        super(studentName, studentID, isEnrolled);
+        this.hasScholarship = hasScholarship;
+        this.scholarshipAmount = scholarshipAmount;
+        this.coursesEnrolled = coursesEnrolled; 
+    }
+    
+    public boolean isHasScholarship(){
+        
+    }
+    
+    public double getScholarshipAmount(){
+        return scholarshipAmount;
+    }
+    
+    public int getCoursesEnrolled(){
+        return coursesEnrolled;
+    }
+    
+    public double getPayableAmount(){
+        
+    }
+    
+    public String toString(){
+        
+    }
+       
 }
