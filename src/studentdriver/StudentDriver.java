7d9524aeaafe266ae683eca4c46a1fe5f73d9b4c
marissah05 <158322080+marissah05@users.noreply.github.com>
2024-04-08 16:47:37 -0500

    selection changes

--- src/studentdriver/StudentDriver.java
+++ src/studentdriver/StudentDriver.java
@@ -1,34 +1,52 @@
 package studentdriver;
+
 import java.util.*;
 import java.io.*;
+
 public class StudentDriver {
 
     public static void main(String[] args) throws FileNotFoundException {
         System.out.println("Project02");
-        
+
         Scanner input = new Scanner(System.in);
         File inputFile = new File("input.csv");
-        System.out.println("Enter the no of UG students: ");
+        System.out.print("Enter the no of UG students: ");
         int numUG = input.nextInt();
-        System.out.println("Enter the no of Graduate students: ");
+        System.out.print("Enter the no of Graduate students: ");
         int numGS = input.nextInt();
-        System.out.println("Enter the no of online students: ");
+        System.out.print("Enter the no of online students: ");
         int numOS = input.nextInt();
-        
+
         StudentFees[] students = new StudentFees[12];
         Scanner inputf = new Scanner(inputFile);
+
         int i = 0;
-        while (inputf.hasNext()){ 
-            String line = input.nextLine();
+
+        while (inputf.hasNext()) {
+            String line = inputf.nextLine();
             String[] lineSplit = line.split(",");
-            if (i < numUG){
+            if (i < numUG) {
                 int id = Integer.parseInt(lineSplit[0]);
+                //String name = String.parseString(lineSplit[1]);
+                //boolean isEnrolled = boolean.parse(lineSplit[]);
+                int coursesEnrolled = Integer.parseInt(lineSplit[3]);
+                //boolean hasScholarship = boolean.parse(lineSplit[2]);
+                //Double scholarshipAmount = double.parse(lineSplit[]);
+            }
+            if (i < numGS) {
+                int id = Integer.parseInt(lineSplit[0]);
+                //String name = String.parseString(lineSplit[1]);
+                //boolean isEnrolled = boolean.parse(lineSplit[]);
+            }
+            if (i < numOS) {
+                int id = Integer.parseInt(lineSplit[0]);
+                //String name = String.parseString(lineSplit[1]);
+                //boolean isEnrolled = boolean.parse(lineSplit[]);
             }
             i++;
-            
+
         }
-        
-         
+
     }
 
 }
